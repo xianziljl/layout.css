8181522020ece46d2b67503161a6891edd03579c
zhuxianyong <zhuxianyong@eversec.cn>
2021-05-12 23:54:06 +0800

    字体大小调整

--- build.js
+++ build.js
@@ -9,9 +9,7 @@ function run() {
 
   fs.readdir(path, (err, files) => {
     if (!err && files && files.length) {
-      files.forEach(file => {
-        fixClassNames(join(path, file))
-      })
+      files.forEach(file => fixClassNames(join(path, file)))
     }
   })
 }
@@ -19,28 +17,15 @@ function run() {
 function fixClassNames(path) {
   fs.readFile(path, 'utf-8', (err, _data) => {
     const arr = _data.split('\n').filter(item => item)
-    console.log(arr)
-    // const match = data.match(/\n?\s*\.[\w-.\\:]+(\s|\,)/g)
-    // if (match) {
-    //   match.forEach(item => {
-    //     console.log(item)
-    //     // let str = item.replace(/\n\s*\./, '')
-    //     // str = str.replace('.', '\\.')
-    //     // str = '\n.' + str
-    //     // // console.log(str)
-    //     // data = data.replace(item, str)
-    //   })
-    // }
-    // console.log(data)
-    // match.forEach(item => {
-    //   console.log(item || '-')
-    // })
-    // const arr = data.split(/\n/)
-    // arr.forEach(str => {
-    //   if (str.length) {
-    //     let className = str.match(/^\s*\.[\w-.\\:]+\s*\{/)
-    //     console.log(str, className ? className[0] : 'null')
-    //   }
-    // })
+    const newArr = arr.map(str => {
+      const match = str.match(/\w+-\d+\./)
+      if (!match) return str
+      return str.replace(match[0], match[0].replace('.', '\\.'))
+    })
+    const data = newArr.join('\n');
+    
+    fs.writeFile(path, data, 'utf-8', () => {
+      console.log(`write file ${path}`)
+    })
   })
 }
