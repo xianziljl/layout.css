d71c30b2b896cf045ace67c7715decbaf4e2a94d
zhuxianyong <zhuxianyong@eversec.cn>
2021-05-14 22:01:45 +0800

    增加了purge命令

--- purge.js
+++ purge.js
@@ -1,41 +1,54 @@
 const fs = require('fs')
 const { join } = require('path')
 
-// run()
+const files = fs.readdirSync('dist')
+const usedNames = getUsedNames()
+files.forEach(f => {
+  const path = join(__dirname, 'dist', f)
+  purgeFile(path, usedNames)
+})
 
-// const files = fs.readdirSync('dist')
-// files.forEach(f => {
-//   const path = join(__dirname, 'dist', f)
-//   const data = fs.readFileSync(path, 'utf-8')
-//   const arr = data.split('\n')
-//   const map = []
-//   arr.forEach((str, index) => {
-//     const cls = str.match(/^\s*[\w-:\.\\]+\s/)
-//     if (!cls) return
-//     const name = cls[0]
-//       .replace(/\s/g, '')
-//       .replace(/^\./, '')
-//       .replace(/\\/g, '')
-//       .replace(':focus', '')
-//       .replace(':hover', '')
-//       .replace(':active', '')
-//     map.push({ name, index })
-//   })
-// })
-// fs.writeFile('x.txt', Array.from(all).join('\n'), 'utf-8', () => {})
 
-function run() {
+function getUsedNames() {
+  const names = new Set()
   const path = join(__dirname, './')
-  fs.readdir(path, (err, files) => {
-    const htmlFiles = files.filter(f => f.match(/\.html$/))
-    htmlFiles.forEach(f => purge(f))
+  const htmlFiles = fs.readdirSync(path).filter(f => f.match(/\.html$/))
+  htmlFiles.forEach(f => {
+    const data = fs.readFileSync(join(path, f), 'utf-8')
+    const match = data.match(/[^<>"'`\s]*[^<>"'`\s:]/g).map(x => x)
+    match.forEach(name => names.add(name))
   })
+  return names
 }
 
-function purge(path) {
-  fs.readFile(path, 'utf-8', (err, _data) => {
-    console.log(path)
-    const used = new Set(_data.match(/[^<>"'`\s]*[^<>"'`\s:]/g).map(x => x))
-    console.log(used)
+function purgeFile(path, usedNames) {
+  const data = fs.readFileSync(path, 'utf-8')
+  const arr = data.split('\n')
+  const map = []
+  arr.forEach((str, index) => {
+    const cls = str.match(/^\s*[\w-:\.\\]+\s/)
+    if (!cls) return
+    const name = cls[0]
+      .replace(/\s/g, '')
+      .replace(/^\./, '')
+      .replace(/\\/g, '')
+      .replace(':focus', '')
+      .replace(':hover', '')
+      .replace(':active', '')
+    map.push({ name, index })
   })
+
+  map.forEach(({name, index}) => {
+    if (!usedNames.has(name)) {
+      arr[index] = ''
+    }
+  })
+  const str = arr
+    .filter(s => s)
+    .join('\n')
+    .replace(/@media.*\n\}/g, '')
+    .replace(/\n+/g, '\n')
+  fs.writeFileSync(path, str, 'utf-8')
 }
+
+
