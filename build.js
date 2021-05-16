d48d3e0690643ade23283e60bb95908d2e037532
zhuxianyong <zhuxianyong@eversec.cn>
2021-05-16 17:05:24 +0800

    减少文件个数，优化打包

--- build.js
+++ build.js
@@ -14,19 +14,33 @@ function run() {
 
 function fixClassNames(path) {
   fs.readFile(path, 'utf-8', (err, _data) => {
-    const arr = _data.split('\n').filter(item => item)
+    const arr = _data
+      .replace(/\}\s\}/g, '}\n}')
+      .replace(/\)\s\{\s\./g, ') {\n  .')
+      .split('\n').filter(item => item)
+
     const newArr = arr.map(str => {
       const match = str.match(/\w+-\d+\./)
       if (!match) return str
       return str.replace(match[0], match[0].replace('.', '\\.'))
     })
-    let data = newArr.join('\n');
-    data = data
-      .replace(/\}\s\}/g, '}\n}')
-      .replace(/\)\s\{\s\./g, ') {\n  .')
+    const data = fixMediaQueryToEnd(newArr).join('\n');
     
     fs.writeFile(path, data, 'utf-8', () => {
       console.log(`write file ${path}`)
     })
   })
+}
+
+function fixMediaQueryToEnd(arr) {
+  const lines = []
+  const mediaLines = []
+  let isMedia = false
+  arr.forEach(str => {
+    if (str.match(/^@media/)) isMedia = true
+    if (isMedia) mediaLines.push(str)
+    else lines.push(str)
+    if (str === '}') isMedia = false
+  })
+  return lines.concat(mediaLines)
 }
